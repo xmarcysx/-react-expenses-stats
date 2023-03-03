57013a7331fe777d7578fef7244b2456fc5a16b8
xmarcysx <marcinchowaniec21@gmail.com>
2023-03-03 21:34:33 +0100

    New ExpenseDate component to have data calendar also with CSS

--- src/App.js
+++ src/App.js
@@ -32,12 +32,12 @@ function App() {
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
-        date={expenses[0].date}
+        date={expenses[1].date}
       />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
-        date={expenses[0].date}
+        date={expenses[2].date}
       />
     </div>
   );
