4fae840f4844fae039ca2cd429b2f3bb2029746f
xmarcysx <marcinchowaniec21@gmail.com>
2023-03-03 13:18:54 +0100

    ExpenseItem: learned style and props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Car Insurance',
+      amount: 286.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e2',
+      title: 'Toilet paper',
+      amount: 10.67,
+      date: new Date(2021, 2, 1),
+    },
+    {
+      id: 'e3',
+      title: 'Mc donald',
+      amount: 12.22,
+      date: new Date(2021, 2, 30),
+    },
+  ];
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[0].date}
+      />
     </div>
   );
 }
